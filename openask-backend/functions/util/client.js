19469078c099a49cd97b5e0b352e516746e879de
xuanzhenzhang <xuanzhenzhang6@gmail.com>
2023-02-26 11:23:56 -0700

    clean

--- openask-backend/functions/util/client.js
+++ openask-backend/functions/util/client.js
@@ -1,9 +1,11 @@
+require("dotenv").config({ path: require("find-config")(".env") });
+
 const { TwitterApi } = require("twitter-api-v2");
 const twClient = new TwitterApi({
-  appKey: "syY1dfeF84qwiYiqD38Tal4SV",
-  appSecret: "If4vQf5PpfCW9meGVM09qMR6TTL9YMG0vxcB7hpgxJnM1FBMMI",
-  accessToken: "1504363325886328835-NbPITuTE86AUDlyBYB0CHblM3ZKFKa",
-  accessSecret: "hrlUmkpVHrd6P3vX7KycQyyoxEG5dFeVapoVy3ie3URKi",
+  appKey: process.env.TWITTER_APP_KEY,
+  appSecret: process.env.TWITTER_APP_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessSecret: process.env.TWITTER_ACCESS_SERECT,
 });
 
 module.exports = { twClient };
