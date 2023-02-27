b7e2554d7a99e0c25d249d3d160d753689bc81e2
Ray Zhu <rayzhudev@gmail.com>
2023-02-27 14:31:15 -0700

    Update gitignore

--- ethereum/scripts/deploy.ts
+++ ethereum/scripts/deploy.ts
@@ -2,10 +2,12 @@ const hre = require("hardhat");
 require('dotenv').config();
 
 async function main() {
-  const [deployer] = await hre.ethers.getSigners();
-  const privateKey = process.env.PRIVATE_KEY;
-  const contractFactory = await hre.ethers.getContractFactory("StandardBounties");
-  const contract = await contractFactory.connect(new ethers.Wallet(privateKey)).deploy();
+    const [deployer] = await hre.ethers.getSigners();
+//   const privateKey = process.env.PRIVATE_KEY;
+    const contractFactory = await hre.ethers.getContractFactory("StandardBounties");
+    const contract = await contractFactory.deploy();
+    await contract.deployed();
+//   const contract = await contractFactory.connect(new ethers.Wallet(privateKey)).deploy();
 
   console.log("Contract deployed to address:", contract.address);
 }
