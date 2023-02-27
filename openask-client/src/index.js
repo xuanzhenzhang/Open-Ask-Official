7421dd7b4c1c02fe46c3800893b7dd2ca3541bc0
Rice <112427358+ricefarmernft@users.noreply.github.com>
2023-02-27 14:08:13 -0600

    added packages and gitignore

--- openask-client/src/index.js
+++ openask-client/src/index.js
@@ -12,6 +12,15 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBxzkOmWASUN7LnXqvFgC40Qrc1pffKxdI",
+  authDomain: "fir-quickask.firebaseapp.com",
+  projectId: "fir-quickask",
+  storageBucket: "fir-quickask.appspot.com",
+  messagingSenderId: "990063199875",
+  appId: "1:990063199875:web:08a54ce8833256da53fdb6",
+  measurementId: "G-4CZB3NP94L",
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
