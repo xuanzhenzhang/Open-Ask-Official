829fefd146c58ea9d7abe15b928c5bf2d74e3de3
Ray Zhu <rayzhudev@gmail.com>
2023-02-27 14:23:40 -0700

    Add standard bounties

---
+++ ethereum/scripts/deploy.ts
@@ -0,0 +1,18 @@
+const hre = require("hardhat");
+require('dotenv').config();
+
+async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  const privateKey = process.env.PRIVATE_KEY;
+  const contractFactory = await hre.ethers.getContractFactory("StandardBounties");
+  const contract = await contractFactory.connect(new ethers.Wallet(privateKey)).deploy();
+
+  console.log("Contract deployed to address:", contract.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
