4149c184a5119398c73c9302503e2d4ae73f6c81
Rice <112427358+ricefarmernft@users.noreply.github.com>
2023-03-03 13:56:18 -0600

    created withdraw function

--- openask-client/src/components/subcomponents/WithdrawButton.js
+++ openask-client/src/components/subcomponents/WithdrawButton.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Box, Typography } from '@mui/material'
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import { withdrawEthPayment } from "../functions/withdrawEthPayment";
 
-const WithdrawButton = () => {
+const WithdrawButton = (props) => {
+  const { contractAddress, bountyId, contributionId } = props;
+  
+  const withdraw = async () => {
+    const receipt = await withdrawEthPayment(
+      contractAddress,
+      bountyId,
+      contributionId
+    );
+    alert("Payment Withdrawn");
+  };
+
   return (
     <Box className="withdraw">
       <Typography>Withdraw</Typography>
     </Box>
-  )
-}
+  );
+};
 
-export default WithdrawButton
+export default WithdrawButton;
