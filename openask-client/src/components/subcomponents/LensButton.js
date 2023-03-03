cad42b4d3478df6340d939dc84f3d332edb11947
Rice <112427358+ricefarmernft@users.noreply.github.com>
2023-03-02 22:54:42 -0600

    added lens button

---
+++ openask-client/src/components/subcomponents/LensButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const LensButton = () => {
+  return (
+    <Box className="lens-btn">
+      <Typography>Post To Lens</Typography>
+    </Box>
+  );
+};
+
+export default LensButton;
