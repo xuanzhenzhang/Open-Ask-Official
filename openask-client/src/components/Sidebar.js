659448c2a5e25f614775bce75e359289e74130fe
Rice <112427358+ricefarmernft@users.noreply.github.com>
2023-03-11 17:03:31 -0600

    fixed sidebar rendering

--- openask-client/src/components/Sidebar.js
+++ openask-client/src/components/Sidebar.js
@@ -1,45 +1,54 @@
 import React from "react";
 import SidebarList from "./SidebarList";
 import Drawer from "@mui/material/Drawer";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import { useTheme } from "@mui/material/styles";
 
 const Sidebar = (props) => {
   const { onAvatarClick, mobileOpen, setMobileOpen } = props;
 
+  const theme = useTheme();
+  const breakpoint = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <>
-      <Drawer
-        className="sidebar"
-        variant="temporary"
-        anchor="right"
-        open={mobileOpen}
-        onClose={onAvatarClick}
-        ModalProps={{
-          keepMounted: true,
-        }}
-        sx={{
-          display: { xs: "flex", sm: "flex", md: "none" },
-          "& .MuiDrawer-paper": {
-            width: "200px",
-          },
-        }}
-      >
-        <SidebarList setMobileOpen={setMobileOpen} mobileOpen={mobileOpen} />
-      </Drawer>
+      {breakpoint && (
+        <Drawer
+          className="sidebar"
+          variant="temporary"
+          anchor="right"
+          open={mobileOpen}
+          onClose={onAvatarClick}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "flex", sm: "flex", md: "none" },
+            "& .MuiDrawer-paper": {
+              width: "200px",
+            },
+          }}
+        >
+          <SidebarList setMobileOpen={setMobileOpen} mobileOpen={mobileOpen} />
+        </Drawer>
+      )}
 
-      <Drawer
-        className="sidebar"
-        variant="permanent"
-        anchor="right"
-        sx={{
-          display: { xs: "none", sm: "none", md: "flex" },
-          "& .MuiDrawer-paper": {
-            width: "300px",
-          },
-        }}
-        open
-      >
-        <SidebarList />
-      </Drawer>
+      {!breakpoint && (
+        <Drawer
+          className="sidebar"
+          variant="permanent"
+          anchor="right"
+          sx={{
+            display: { xs: "none", sm: "none", md: "flex" },
+            "& .MuiDrawer-paper": {
+              width: "300px",
+            },
+          }}
+          open
+        >
+          <SidebarList />
+        </Drawer>
+      )}
     </>
   );
 };
